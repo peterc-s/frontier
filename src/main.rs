a4f5e0929b94215ab8f8d9642742653da71e0e38
peterc-s <pjcsmail@gmail.com>
2024-03-16 23:35:54 +0000

    yay and pacman done

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 use clap::Parser;
+use colored::Colorize;
 
 mod config;
 mod install;
 mod generate_pacman;
+mod generate_yay;
 
 #[derive(Parser)]
 enum Frontier {
     Install(install::Install),
     GeneratePacman(generate_pacman::GeneratePacman),
+    GenerateYay(generate_yay::GenerateYay),
 }
 
 fn main() {
@@ -17,7 +20,15 @@ fn main() {
     // perform specific subcommand
     match args {
         Frontier::Install(install) => install.run(),
-        Frontier::GeneratePacman(generate_pacman) => generate_pacman.run(),
+        Frontier::GeneratePacman(generate_pacman) => {
+                println!("{} creating output file...", "[frontier]".bold().purple());
+                generate_pacman.run();
+                println!("{} success!", "[frontier]".bold().purple());
+            },
+        Frontier::GenerateYay(generate_yay) => {
+                println!("{} creating output file...", "[frontier]".bold().purple());
+                generate_yay.run();
+                println!("{} success!", "[frontier]".bold().purple());
+            },
     }
-
 }
